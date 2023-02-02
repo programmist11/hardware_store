782b033959577987af549e33e41bb361b7d09e37
anton <nagulin03"mail.ru>
2023-02-02 19:43:40 +0300

    refactor models.py

--- shop/models.py
+++ shop/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=200, verbose_name='Наименование')
+    descriptions = models.TextField(verbose_name='Описание')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name="Фото",)
 
     def __str__(self):
         return self.title
